59f9caba821d397c7ce66044c9fd120c2e0ecead
Hector Garzon <hferney@technisys.com>
2022-07-18 16:44:23 -0500

    Change background image feature

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,10 +1,23 @@
-import { FC, PropsWithChildren } from 'react';
+import { FC, PropsWithChildren, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import { NavRoutes } from '@/components';
 import { LayoutContainer, LayoutContent } from './MainLayout.styles';
+import { routes, RoutesEnum } from '@/models';
 
-export const MainLayout: FC<PropsWithChildren> = ({ children }) => (
-  <LayoutContainer>
-    <NavRoutes />
-    <LayoutContent>{children}</LayoutContent>
-  </LayoutContainer>
-);
+export const MainLayout: FC<PropsWithChildren> = ({ children }) => {
+  const { route } = useRouter();
+
+  useEffect(() => {
+    const backgroundUrl = `url(${
+      routes[route]?.background || routes[RoutesEnum.HOME].background
+    })`;
+    document.body.style.backgroundImage = backgroundUrl;
+  }, [route]);
+
+  return (
+    <LayoutContainer>
+      <NavRoutes />
+      <LayoutContent>{children}</LayoutContent>
+    </LayoutContainer>
+  );
+};
